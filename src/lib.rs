e541954c4894108e10023dacc1ee5e9d1ead8b5d
kc1212 <kc1212@users.noreply.github.com>
2017-03-31 20:27:13 +0200

    Remove unused crates

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,6 @@ pub mod node;
 
 extern crate futures;
 extern crate tokio_core;
-extern crate tokio_proto;
-extern crate tokio_service;
 extern crate tokio_timer;
 extern crate uuid;
 extern crate serde;
